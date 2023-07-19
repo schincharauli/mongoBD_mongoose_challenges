32844d7b36530f89f413bde380e8b5551725a33b
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-07-19 22:11:21 +0400

    build the project

--- myApp.js
+++ myApp.js
@@ -1,7 +1,23 @@
-require('dotenv').config();
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 let Person;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+name: {
+  type: String;
+}
+age: {
+  type: Number;
+}
+favoriteFoods {
+  type: [String]
+}
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
